8856935133bc635dcdde078f83f871021deeb9a0
TW Webb <twwebb1@me.com>
2024-09-09 21:57:54 -0400

    resume details added.

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, ProfilePageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("profile/", ProfilePageView.as_view(), name="profile"),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,6 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
+
+class ProfilePageView(TemplateView):
+    template_name = 'profile.html'
